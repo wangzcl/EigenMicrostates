47fde4b94cd1dc04381b2b39ea49498c87e4a6f9
Documenter.jl <documenter@juliadocs.github.io>
2024-10-06 18:47:06 +0000

    build based on 176be33

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = EigenMicrostates","category":"page"},{"location":"#EigenMicrostates","page":"Home","title":"EigenMicrostates","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for EigenMicrostates.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [EigenMicrostates]","category":"page"}]
+}
